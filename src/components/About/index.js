1ad1adb4280c664bb62e47cc2ea73f5acd129491
demuelle <dmueller@trilogyed.com>
2022-02-17 10:27:16 -0600

    Adds about component

---
+++ src/components/About/index.js
@@ -0,0 +1,10 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+const About = () => (
+    <section className="my-5">
+        <h1 id="about">Whos am I?</h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+    </section>
+)
+
+export default About;
