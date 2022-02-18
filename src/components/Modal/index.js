9ccf4b4591917daaab16bfc132030f95440b4e3f
demuelle <dmueller@trilogyed.com>
2022-02-18 13:04:59 -0600

    Fixes description of photo in modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -5,7 +5,7 @@ function Modal({photo, toggleModal}) {
       <div className="modalContainer">
         <h3 data-testid="modal-title" className="modalTitle">{name}</h3>
         <img src={require(`../../assets/large/${category}/${index}.jpg`)} alt={description} />
-        <p>Photo Description</p>
+        <p>{description}</p>
         <button onClick={toggleModal} type="button">Close this modal</button>
       </div>
     </div>
